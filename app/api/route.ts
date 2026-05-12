50c5192cbde14b387a81d03cdbd34d5957d58a46
FilipRank <42890466+FilipRank@users.noreply.github.com>
2026-05-12 10:00:55 +0200

    remove nextauth

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,4 @@
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "./auth/[...nextauth]/route";
 
 const apiDocumentation = {
     title: "Bluehorse REST API",
