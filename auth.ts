5c6b16ef4759449816e30c2ede35052b93731a8c
FilipRank <42890466+FilipRank@users.noreply.github.com>
2026-05-12 12:21:14 +0200

    adapt schema to authjs

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./lib/prisma";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
     debug: process.env.AUTH_DEBUG === "true",
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: "jwt" },
 });
